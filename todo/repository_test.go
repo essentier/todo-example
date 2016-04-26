85dc1890bb3fbfcaef47054fecdfd413ed54ef98
chaurwu <wu.chaur@gmail.com>
2016-04-26 08:52:28 -0700

    rename the filename of the mgo middleware

--- todo/repository_test.go
+++ todo/repository_test.go
@@ -8,21 +8,14 @@ import (
 )
 
 func TestSaveNew(t *testing.T) {
-	//t.Parallel()
-	// originalVal := os.Getenv("SPICKSPAN_MODE")
-	// defer os.Setenv("SPICKSPAN_MODE", originalVal)
-	// os.Setenv("SPICKSPAN_MODE", "local")
-
-	// repo, dbSession := getRepo(t)
-	// defer dbSession.Close()
-
+	t.Parallel()
 	mgoService := testutil.CreateMgoService("todo-db", t)
+	defer mgoService.Release()
 	dbSession, err := db.CreateDBSession(mgoService.GetUrl())
 	handleFatalError(t, "Failed to create DB session.", err)
 	defer dbSession.Close()
 
-	db := dbSession.DB("tododb")
-	repo := getRepository(db)
+	repo := getRepository(dbSession.DB("tododb"))
 	todo := Todo{Name: "todo1", Completed: false}
 	savedTodo, err := repo.saveNew(todo)
 	handleFatalError(t, "Failed to find todo by ID.", err)
@@ -33,23 +26,6 @@ func TestSaveNew(t *testing.T) {
 		t.Errorf("Expect todo's name to be todo1 but got %v", retrievedTodo.Name)
 	}
 }
-
-// func getRepo(t *testing.T) (repository, *mgo.Session) {
-// 	provider, err := spickspan.GetDefaultServiceProvider()
-// 	handleFatalError(t, "Failed to get spickspan provider.", err)
-
-// 	mgoService, err := spickspan.GetMongoDBService(provider, "todo-db")
-// 	handleFatalError(t, "Failed to get MongoDB service.", err)
-
-// 	mgoUrl := mgoService.IP + ":" + strconv.Itoa(mgoService.Port)
-// 	dbSession, err := createDBSession(mgoUrl)
-// 	handleFatalError(t, "Failed to create DB session.", err)
-
-// 	reqSession := dbSession.Clone()
-// 	db := reqSession.DB("tododb")
-// 	repo := getRepository(db)
-// 	return repo, reqSession
-// }
 
 func handleFatalError(t *testing.T, message string, err error) {
 	if err != nil {
